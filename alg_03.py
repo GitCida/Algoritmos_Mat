f1ed4bc133f7a78fa18de38e5508fb38380e0270
GitCida <mariaaparecidaily2@gmail.com>
2024-08-09 20:11:18 -0300

    Feito o código do alg_03

---
+++ alg_03.py
@@ -0,0 +1,7 @@
+cat = float(input('Digite qual é o valor do cateto que se tem o valor: '))
+hip = float(input('Agora, digite o valor da hipotenusa: '))
+result1 = (hip**2 - cat**2)
+import math
+result2 = math.sqrt(result1)
+print(f' O valor do cateto que está faltando é: {result2}')
+#Teorema de Pitágoras(Cateto)
